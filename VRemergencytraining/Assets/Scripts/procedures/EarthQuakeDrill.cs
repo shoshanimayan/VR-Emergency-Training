addd249d75b8ccabfd196b5b3a505c013c31afe0
mayanshoshani-karuna <mayan.shoshani@karunalabs.com>
2021-10-16 19:08:44 -0700

    update gitignore

---
+++ VRemergencytraining/Assets/Scripts/procedures/EarthQuakeDrill.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EarthQuakeDrill : Procedure
+{
+    /////////////////// 
+    //private variables
+    ///////////////////
+    [SerializeField] private GameManager _manager;
+    [SerializeField] private GameObject _earthQuakeProps;
+    private string _instruction = "for your saftey get under a table during an earth quake";
+
+    /////////////////// 
+    //private methods
+    //////////////////
+    private void Awake()
+    {
+        running = false;
+        _earthQuakeProps.SetActive(false);
+    }
+
+    /////////////////// 
+    //public API
+    //////////////////
+    public bool running;
+    public override void Off() 
+    {
+        _earthQuakeProps.SetActive(false);
+        running = false;
+        float t = GameManager.time;
+        float minutes = Mathf.Floor(t / 60);
+        float seconds = Mathf.RoundToInt(t % 60);
+        string min;
+        string sec;
+        if (minutes < 10)
+        {
+            min = "0" + minutes.ToString();
+        }
+        else { min = minutes.ToString(); }
+        if (seconds < 10)
+        {
+            sec = "0" + Mathf.RoundToInt(seconds).ToString();
+        }
+        else { sec = Mathf.RoundToInt(seconds).ToString(); }
+        _manager.afterText = "Completed Earth Quake training\nTime: " + min + ":" + sec + "\nHints found: " + GameManager.hintsChecked.Count.ToString() + "/" + GameManager.hintsTotal.ToString();
+        _manager.Off();
+
+    }
+
+    public override void Initiate()
+    {
+        _manager.On(2,"earthquake");
+        _manager.TextUpdate(_instruction);
+        _earthQuakeProps.SetActive(true);
+        running = true;
+        _manager.music.EarthQuakeDrillAudio();
+
+
+    }
+}
---
+++ VRemergencytraining/Assets/Scripts/procedures/musicManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class musicManager : MonoBehaviour
+{
+    /////////////////// 
+    //private Variables
+    ///////////////////
+    private AudioSource _aS;
+    [SerializeField] private AudioClip _fireAlarm;
+    [SerializeField] private AudioClip _EarthQuakeAlarm;
+
+
+    /////////////////// 
+    //private methods
+    //////////////////
+    private void Awake()
+    {
+        _aS = GetComponent<AudioSource>();
+    }
+
+    /////////////////// 
+    //public API
+    //////////////////
+    public void FireDrillAudio() {
+        _aS.clip = _fireAlarm;
+        _aS.Play();
+    }
+
+    public void EarthQuakeDrillAudio()
+    {
+        _aS.clip = _EarthQuakeAlarm;
+        _aS.Play();
+    }
+
+    public void StopAudio()
+    {
+        _aS.Stop();
+        
+    }
+}
